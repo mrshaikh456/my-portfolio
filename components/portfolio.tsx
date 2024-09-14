d8c143e60b446e17c719dab5c3025a5657899b88
mrshaikh456 <89177088+mrshaikh456@users.noreply.github.com>
2024-09-14 22:14:48 +0530

    Update portfolio.tsx
    updated errors for unused commands

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
